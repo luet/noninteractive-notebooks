c76a34541e9d3eef0268b8e6904def74c34fd70b
David Luet <luet@princeton.edu>
2022-09-21 13:37:00 -0400

    Fix typo

--- submit.sh
+++ submit.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 #SBATCH -N 1
 #SBATCH -n 1
-#SBATCH -t 00:10:00
+#SBATCH -t 00:10:00              # this is for 10 min
 #SBATCH --mail-type=begin        # send email when job begins
 #SBATCH --mail-type=end          # send email when job ends
 #SBATCH --mail-user=<YourNetID>@princeton.edu
